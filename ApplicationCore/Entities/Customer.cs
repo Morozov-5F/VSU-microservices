5d2ec5db4a42fb552c851c8723f28a07a54c4f8d
Eugene Morozov <zhe3145@yandex.ru>
2017-11-26 13:46:21 +0300

    Initial commit: - Basic solution structure - Basic dependencies - Logging system

---
+++ ApplicationCore/Entities/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ApplicationCore.Entities
+{
+    public class Customer
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public UInt16 Age { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+    }
+}
---
+++ Infrastructure/Data/MunicipalContext.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data
+{
+    public class MunicipalContext : DbContext
+    {
+        public MunicipalContext(DbContextOptions<MunicipalContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(ConfigureCustomer);
+        }
+
+        private void ConfigureCustomer(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("Customers");
+
+            // TODO: Customer configuration. Look up WTF is that.
+        }
+    }
+}
